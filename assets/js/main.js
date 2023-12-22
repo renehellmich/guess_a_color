94af09c8a7281437917f367f0bced6a7c3de682c
renehellmich <renehellmich94@gmail.com>
2023-12-22 13:28:10 +0100

    completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,6 @@ const spanArr = document.querySelectorAll("#span_color")
 let rightDiv = 0;
 
 let rgb = [0, 0, 0]
-let rgbDiv = [0, 0, 0]
 
 console.log(divArr);
 console.log(spanArr);
@@ -34,25 +33,50 @@ getColor()
 
 const divRandomNum = () => {
     number = Math.ceil(Math.random() * 5);
-    return number;
+    return number - 1;
 }
 
-const generateDivColor = (value) => {
-    console.log(value);
-    rightDiv = value;
+const generateDivColor = () => {
+    // console.log(rgb);
+    // console.log(value);
+    rightDiv = divRandomNum();
     divArr.forEach((div, i) => {
         let color = 0
-        if (value != i - 1) {
+        if (rightDiv != i) {
             color = "rgb(" + rgbGenerate() +","+rgbGenerate()+"," + rgbGenerate() + ")";
-            console.log(color);
+            // console.log(color);
             divArr[i].style.background = color
+            // divArr[i].innerText = color;
         } else {
-            color = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`
-            divArr[value] = color;
+            color = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
+            // console.log(color);
+            divArr[rightDiv].style.background = color
+            // divArr[rigthDiv].innerText = color;
         }
     });
 }
 
-generateDivColor(divRandomNum())
-console.log(rgb);
-rgbGenerate()
+generateDivColor()
+
+for(let div of divArr) {
+    console.log(div.style.background);
+    const rgbValue = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`
+
+    div.addEventListener("click", (event) => {
+        console.log(event.target.style.background);
+        if (event.target.style.background != rgbValue) {
+            output.style.display = "block"
+            output.innerText = "Das war leider die falsche Farbe. Bitte drücke Reset und versuche es erneut."
+            console.log("Falsch");
+        } else {
+            output.innerText = "Du bist ein RGB Genie"
+            output.style.display = "block"
+            console.log("Richtig");
+        }
+    })
+}
+
+function resetMe() {
+    getColor();
+    generateDivColor();
+}
